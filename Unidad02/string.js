89b8588201fe7ae7475a9d2edddbb347c114231f
sebastiancordova-sudo <sebastian.cordova@unl.edu.ec>
2026-06-12 18:25:28 +0000

    feat: primer trabajo javascript

---
+++ Unidad02/string.js
@@ -0,0 +1,15 @@
+let string1= 'Hola mundo'
+let string2= "Javascript es genial"
+let string3= `${string1} feliz:)`
+let string4= string1 + ' ' + string2
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
+
+let frase = 'JavaScript es Genial'
+console.log(frase.length) // longitud de la cadena
+console.log(frase.toUpperCase()) // convierte a mayusculas
+console.log(frase.toLowerCase()) // convierte a minusculas
+console.log(frase.substring(0,10)) // extrae una parte de la cadena
---
+++ Unidad02/tipos-datos.js
@@ -0,0 +1,36 @@
+//PRIMITIVOS
+let nombre = "Juan";
+
+//NUMERICO
+let edad = 30;
+
+//BOOLEANO
+let esMayorDeEdad = true
+
+//NULL
+let noHayValor = null
+
+//UNDEFINED
+let noDefinido = undefined
+
+//SYMBOL valor unico
+let simboloUnico = Sumbol("unico")
+
+//DATOS GIGANTES bigint
+let numeroGrande = 2n
+
+//COMPLETOS
+//OBJETOS
+let carro = {
+    marca: "Toyota",
+    modelo: "Fortuner",
+    año: 2020
+}
+
+//ARREGLOS
+let frutas = ["Manzana", "Banana", "Naranja"];
+
+//FUNCIONES
+function saludar() {
+    console.log("Hola");
+}
