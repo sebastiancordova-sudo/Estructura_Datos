37e20e491565a367e2ee95e8cb3d52bbbfac9374
sebastiancordova-sudo <sebastian.cordova@unl.edu.ec>
2026-06-16 17:29:54 -0500

    Clase del 16 de Junio del 2026

---
+++ Unidad02/clase-conversion.js
@@ -0,0 +1,17 @@
+const string = "47"
+const integer = parseInt(string)
+console.log(integer)
+console.log(typeof integer)
+
+const stringDecimal = "3.14"
+const decimal = parseFloat(stringDecimal)
+console.log(decimal)
+console.log(typeof decimal)
+
+const binary = '10010' 
+const decimal = parseInt(binary, 2)
+console.log(decimal)
+
+const suma = "5" - true
+console.log(suma)
+
---
+++ Unidad02/clase-number.js
@@ -0,0 +1,39 @@
+//Tipos de datos numericos 
+
+//entero y decimal
+
+const entero = 42
+const decimal = 3.14 
+console.log(typeof entero, typeof decimal)
+
+//Notacion cientifica 
+const cientifico = 5e3
+
+//Representacion de otros contextos.
+const infinito = Infinity
+const noEsUnNumero = NaN
+
+
+//Operaciones Aritmeticas 
+
+
+//Sumas Restas 
+const suma = 3 + 4 
+const resta = 4 - 4 
+const multiplicacion = 4 * 7
+const division = 10 / 2 
+
+//Modulos de exponenciacion
+const modulo = 15 % 8
+const exponenciacion = 0.1 + 0.2 
+
+console.log(exponenciacion)
+console.log(exponenciacion.toFixed(1))
+
+const raizCuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const aleatorio = Math.random()
+
+console.log(raizCuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
